64be1294371879a8d666269bbd60ccbf3598a50e
Jay Nagpaul <jaynagpaul@users.noreply.github.com>
2024-04-05 20:57:53 -0400

    working? Co-authored-by: Tejas Mehta <tmthecoder@gmail.com>

--- gh-api.py
+++ gh-api.py
@@ -1,13 +1,15 @@
 from ghapi.all import GhApi
 from flask import Flask, request
-from ai import build_howto
+from .ai import build_howto
 from ghapi.all import GhApi
+import time
+from jwt import PyJWT
 import os
 
-secret = os.getenv("GH_SECRET")
+SECRET = os.getenv("GH_SECRET")
+APP_ID = os.getenv("GH_APP_ID")
 
 app = Flask(__name__)
-api = GhApi(token=secret)
 
 BITBOT_STR = "@samrat"
 BOG_NAME = "GTBitsOfGood"
@@ -27,7 +29,9 @@ def read_incoming_webhook():
     repo_url = gen_repo_url(repository['full_name'])
 
     owner = repo_path_components[0]
-    repo_name = repo_path_components[1];
+    repo_name = repo_path_components[1]
+
+
 
     if BOG_NAME not in repo_name:
         return
@@ -39,6 +43,7 @@ def read_incoming_webhook():
     issue_id = issue['id']
     issue_title = issue['title']
 
+    create_comment(owner, repo_name, issue_id, "Generating how-to...")
     comment_body = build_howto(name, issue_title, gen_repo_url(repo_name))
 
     create_comment(owner, repo_name, issue_id, comment_body)
@@ -47,4 +52,18 @@ def gen_repo_url(name):
     return f'https://github.com/{name}'
 
 def create_comment(owner, repo, issue, comment):
+    api = GhApi(token=gen_jwt())
     api.issues.create_comment(owner, repo, issue, comment)
+
+def gen_jwt():
+    payload = {
+        # Issued at time
+        'iat': int(time.time()),
+        # JWT expiration time (10 minutes maximum)
+        'exp': int(time.time()) + 600,
+        # GitHub App's identifier
+        'iss': APP_ID
+    }
+    jwt_ins = PyJWT()
+    encoded_jwt = jwt_ins.encode(payload, SECRET, algorithm='RS256')
+    return encoded_jwt
